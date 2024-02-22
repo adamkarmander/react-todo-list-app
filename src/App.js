bcdc8a6a01c2598e6933aa61df55f94d3e5c62ac
adamkarmander <71024067+adamkarmander@users.noreply.github.com>
2024-02-22 16:10:51 +0100

    Add header and custom font

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <head>
+        <style>
+          @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap')
+        </style>
+      </head>
+      <header>
+        <h1>My Todo List</h1>
+      </header>
     </div>
   );
 }
