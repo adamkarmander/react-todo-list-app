2e2a980f0766bfadb60a9826f6dccf46198a4c53
adamkarmander <71024067+adamkarmander@users.noreply.github.com>
2024-03-05 16:11:25 +0100

    Start working on Todo component

---
+++ src/components/Todo.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Todo() {
+    return(
+        <div className="todo">
+            <li className="todo-item">HEYYY</li>
+            <div className="todo-buttons">
+                <button className="complete-btn">Complete</button>
+                <button className="trash-btn">Delete</button>
+            </div>
+        </div>
+    );
+};
+
+export default Todo;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,15 @@
 import React from "react";
+//Importing Components
+import Todo from './Todo';
 
 function TodoList() {
     return (
         <div className="todo-container">
-            <ul className="todo-list"></ul>
+            <ul className="todo-list">
+                <Todo />
+                <Todo />
+                <Todo />
+            </ul>
         </div>
     );
 };
